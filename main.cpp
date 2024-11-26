1bd69a37766e427a7e96a56c60a98ec7ea81ddfe
ALARNINJAD <alarninjad@gmail.com>
2024-11-26 21:15:17 +0330

    first pull

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <filesystem>
 using namespace std;
+
+void menuWindow();
 
 class user {
 
@@ -37,5 +40,48 @@ class admin : public user {
 
 int main(){
 
+	menuWindow();
     return 0;
+}
+
+void menuWindow(){
+	// Create a window
+	sf::RenderWindow window(sf::VideoMode(1600, 1200), "Othello");
+	window.setFramerateLimit(60);
+
+	string currentPath = filesystem::path(__FILE__).parent_path().string();
+	sf::Font font;
+	if(!font.loadFromFile(currentPath + "/SpaceMono-Bold.ttf"))
+		window.close();
+	std::string str = "Welcome!";
+	int i = 1;
+
+
+	while (window.isOpen()) {
+		sf::Event event{};
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		sf::Text welcomeText;
+		welcomeText.setFont(font);
+		welcomeText.setString(str.substr(0, i++));
+		sleep(sf::seconds(0.25));
+		welcomeText.setCharacterSize(48);
+
+		welcomeText.setFillColor(sf::Color::White);
+
+		// Center the text
+		sf::FloatRect textBounds = welcomeText.getLocalBounds();
+		welcomeText.setOrigin(textBounds.left + textBounds.width / 2.0f,
+					   textBounds.top + textBounds.height / 2.0f);
+		welcomeText.setPosition((float) window.getSize().x / 2, (float) window.getSize().y / 2);
+
+		window.clear();
+
+		window.draw(welcomeText);
+		
+		window.display();
+	}
+	
 }
