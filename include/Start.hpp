9762194798f5e5a4d90a5739437a054235730596
ALARNINJAD <alarninjad@gmail.com>
2024-11-29 21:48:45 +0330

    add a Press Enter to continue animation

--- include/Start.hpp
+++ include/Start.hpp
@@ -6,16 +6,18 @@
 
 class Start : public Game {
 private:
-
     sf::Font font;
-    sf::Text welcomeText;
+    sf::Text othelloText;
+    sf::Text continueText;
     std::string welcomeMessage;
     int messageIndex;
+    bool messageComplete;
+    bool fadeInProgress;
+    float fadeProgress;
 
     void initStart();
 
 public:
-
     Start();
     virtual ~Start();
 
--- src/Start.cpp
+++ src/Start.cpp
@@ -1,10 +1,15 @@
 #include "../include/Start.hpp"
 
-Start::Start() { this->initStart(); }
+Start::Start() {
+    this->initStart();
+    this->messageComplete = false;
+    this->fadeInProgress = false;
+    this->fadeProgress = 0.0f; // To track fade progress (from 0 to 1)
+}
+
 Start::~Start() {}
 
 void Start::initStart() {
-    
     if (!this->font.loadFromFile("../assets/fonts/SpaceMono-Bold.ttf")) {
         std::cerr << "Failed to load font!" << std::endl;
         return;
@@ -13,31 +18,64 @@ void Start::initStart() {
     this->welcomeMessage = "Othello";
     this->messageIndex = 1;
 
-    this->welcomeText.setFont(this->font);
-    this->welcomeText.setCharacterSize(80);
-    this->welcomeText.setFillColor(sf::Color::White);
-    this->welcomeText.setPosition((float)this->window->getSize().x / 2, (float)this->window->getSize().y / 2);
-    this->welcomeText.setOrigin(this->welcomeText.getLocalBounds().width / 2.0f, this->welcomeText.getLocalBounds().height / 2.0f);
+    this->othelloText.setFont(this->font);
+    this->othelloText.setCharacterSize(80);
+    this->othelloText.setFillColor(sf::Color::White);
+    sf::FloatRect textBounds = this->othelloText.getLocalBounds();
+    this->othelloText.setOrigin(textBounds.width / 2.0f, textBounds.height / 2.0f);
+    this->othelloText.setPosition((float)this->window->getSize().x / 2, (float)this->window->getSize().y / 2 - 50);
+
+    this->continueText.setFont(this->font);
+    this->continueText.setCharacterSize(40);
+    this->continueText.setFillColor(sf::Color::Black);  // Start with black color
+    this->continueText.setString("Press Enter to continue");
+    sf::FloatRect continueBounds = this->continueText.getLocalBounds();
+    this->continueText.setOrigin(continueBounds.width / 2.0f, continueBounds.height / 2.0f);
+    this->continueText.setPosition((float)this->window->getSize().x / 2, (float)this->window->getSize().y / 2 + 50);
 }
 
 void Start::update() {
-    
     this->pollEvents();
 
+    // Handle the "Othello" message animation
     if (this->messageIndex <= this->welcomeMessage.length()) {
+        this->othelloText.setString(this->welcomeMessage.substr(0, this->messageIndex++));
 
-        this->welcomeText.setString(this->welcomeMessage.substr(0, this->messageIndex++));
+        sf::FloatRect textBounds = this->othelloText.getLocalBounds();
+        this->othelloText.setOrigin(textBounds.width / 2.0f, textBounds.height / 2.0f);
+        this->othelloText.setPosition((float)this->window->getSize().x / 2, (float)this->window->getSize().y / 2 - 50);
 
-        sf::FloatRect textBounds = this->welcomeText.getLocalBounds();
-        this->welcomeText.setOrigin(textBounds.left + textBounds.width / 2.0f,
-                                    textBounds.top + textBounds.height / 2.0f);
-        this->welcomeText.setPosition((float)this->window->getSize().x / 2,
-                                      (float)this->window->getSize().y / 2);
+        sf::sleep(sf::seconds(0.25));
+    }
+    else if (!this->messageComplete) {
+        // Once "Othello" is fully displayed, start the fade-in of "Press Enter to continue"
+        this->messageComplete = true;
+        this->fadeInProgress = true; // Start the fade animation
+    }
+
+    // Fade-in effect for "Press Enter to continue"
+    if (this->fadeInProgress && this->fadeProgress < 1.0f) {
+        this->fadeProgress += 0.01f; // Increment fade progress (you can adjust the speed here)
+        if (this->fadeProgress > 1.0f) {
+            this->fadeProgress = 1.0f; // Ensure it doesn't exceed 1
+        }
+
+        // Update the color of the text (interpolating from black to white)
+        int red = static_cast<int>(255 * this->fadeProgress);
+        int green = static_cast<int>(255 * this->fadeProgress);
+        int blue = static_cast<int>(255 * this->fadeProgress);
+        this->continueText.setFillColor(sf::Color(red, green, blue));
     }
 }
 
 void Start::render() {
     this->window->clear(sf::Color::Black);
-    this->window->draw(this->welcomeText);
+    this->window->draw(this->othelloText);
+
+    // Only draw the continueText if the message is complete and fade-in is in progress
+    if (this->messageComplete) {
+        this->window->draw(this->continueText);
+    }
+
     this->window->display();
 }
