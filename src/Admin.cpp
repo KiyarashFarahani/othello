1d75949eb621231ee29f5f7a03acce867f46d5d6
ALARNINJAD <alarninjad@gmail.com>
2024-12-01 14:33:56 +0330

    make a structure for files

--- src/Admin.cpp
+++
@@ -1,8 +0,0 @@
-#include "../include/Admin.hpp"
-
-admin::admin(int id, string username, string password)
-    : user(id, username, password) {}
-
-void admin::deleteUser() {
-    // Implementation for deleting a user
-}
